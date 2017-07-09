08a0621b3f557ee4e732daf6704d4f222928bbda
Ted Enamorado <fastlinkr@gmail.com>
2017-07-09 13:24:16 +0000

    version 0.1.0

---
+++ R/fastLink-package.R
@@ -0,0 +1,28 @@
+#' Fast Probabilistic Record Linkage with Missing Data
+#'
+#' \code{fastLink} implements methods developed by Enamorado, Fifield, and Imai (2017)
+#' ''Using a Probabilistic Model to Assist Merging of Large-scale Administrative Records'',
+#' to probabilistically merge large datasets using the Fellegi-Sunter model
+#' while allowing for missing data and the inclusion of auxiliary information.
+#' The current version of this package conducts a merge of two datasets under
+#' the Fellegi-Sunter model, using the Expectation-Maximization Algorithm. In addition,
+#' tools for conducting and summarizing data merges are included. 
+#' 
+#' \tabular{ll}{ Package: \tab fastLink\cr Type: \tab Package\cr Version: \tab 0.1.0-\cr
+#' Date: \tab 2017-07-07\cr License: \tab GPL (>= 3)\cr }
+#'
+#' @name fastLink-package
+#' @useDynLib fastLink, .registration = TRUE
+#' @aliases fastLink-package 
+#' @docType package
+#' @author Ted Enamorado \email{fastlinkr@@gmail.com}, Ben Fifield \email{fastlinkr@@gmail.com}, and Kosuke Imai \email{kimai@@princeton.edu}
+#' 
+#' Maintainer: Ted Enamorado \email{fastlinkr@@gmail.com}
+#' @references Enamorado, Ted, Ben Fifield and Kosuke Imai. (2017) "Using a Probabilistic Model to Assist Merging of
+#' Large-scale Administrative Records." Working Paper. Available at \url{http://imai.princeton.edu/research/linkage.html}.
+#' @keywords package
+#' @import Matrix data.table
+#' @importFrom Rcpp evalCpp
+#' @importFrom stats kmeans na.omit prcomp predict quantile var
+#' @importFrom utils data
+NULL
---
+++ R/gammaKpar.R
@@ -0,0 +1,110 @@
+#' gammaKpar
+#'
+#' Field comparisons: 0 disagreement, 2 total agreement.
+#'
+#' @usage gammaKpar(matAp, matBp, gender, n.cores)
+#' 
+#' @param matAp vector storing the comparison field in data set 1
+#' @param matBp vector storing the comparison field in data set 2
+#' @param gender Whether the matching variable is gender. Will override
+#' standard warnings of missingness/nonvariability. Default is FALSE.
+#' @param n.cores Number of cores to parallelize over. Default is NULL.
+#'
+#' @return \code{gammaKpar} returns a list with the indices corresponding to each
+#' matching pattern, which can be fed directly into \code{tableCounts} and \code{matchesLink}.
+#'
+#' @author Ted Enamorado <ted.enamorado@gmail.com>, Ben Fifield <benfifield@gmail.com>, and Kosuke Imai
+#'
+#' @examples
+#' \dontrun{
+#' g1 <- gammaKpar(dfA$birthyear, dfB$birthyear)
+#' }
+#' @export
+
+## ------------------------
+## gamma.k.par
+## This function applies gamma.k
+## in parallel
+## ------------------------
+
+gammaKpar <- function(matAp, matBp, gender = FALSE, n.cores = NULL) {
+
+    ## For visible bindings
+    i <- NULL
+
+    if(any(class(matAp) %in% c("tbl_df", "data.table"))){
+        matAp <- as.data.frame(matAp)[,1]
+    }
+    if(any(class(matBp) %in% c("tbl_df", "data.table"))){
+        matBp <- as.data.frame(matBp)[,1]
+    }
+
+    if(is.null(n.cores)) {
+        n.cores <- detectCores() - 1
+    }
+
+    matAp[matAp == ""] <- NA
+    matBp[matBp == ""] <- NA
+
+    if(!gender){
+        if(sum(is.na(matAp)) == length(matAp) | length(unique(matAp)) == 1){
+            stop("You have no variation in this variable, or all observations are missing in dataset A.")
+        }
+        if(sum(is.na(matBp)) == length(matBp) | length(unique(matBp)) == 1){
+            stop("You have no variation in this variable, or all observations are missing in dataset B.")
+        }
+    }else{
+        if(sum(is.na(matAp)) == length(matAp)){
+            stop("You have no variation in this variable, or all observations are missing in dataset A.")
+        }
+        if(sum(is.na(matBp)) == length(matBp)){
+            stop("You have no variation in this variable, or all observations are missing in dataset B.")
+        }
+    }
+
+    matrix.1 <- as.matrix(as.character(matAp))
+    matrix.2 <- as.matrix(as.character(matBp))
+
+    matrix.1[is.na(matrix.1)] <- "9999"
+    matrix.2[is.na(matrix.2)] <- "9998"
+
+    u.values.1 <- unique(matrix.1)
+    u.values.2 <- unique(matrix.2)
+
+    matches <- u.values.1[u.values.1 %in% u.values.2]
+
+    ht1 <- new.env(hash=TRUE)
+    ht2 <- new.env(hash=TRUE)
+    matches.l <- as.list(matches)
+
+    if(Sys.info()[['sysname']] == "Windows") {
+        nc <- n.cores
+        cl <- makeCluster(nc)
+        registerDoParallel(cl)
+        final.list <- foreach(i = 1:length(matches.l)) %dopar% {
+            ht1 <- which(matrix.1 == matches.l[[i]]); ht2 <- which(matrix.2 == matches.l[[i]])
+            list(ht1, ht2)
+        }
+        stopCluster(cl)
+    } else {
+    	no_cores <- n.cores
+    	final.list <- mclapply(matches.l, function(s){
+            ht1[[s]] <- which(matrix.1 == s); ht2[[s]] <- which(matrix.2 == s);
+            list(ht1[[s]], ht2[[s]]) }, mc.cores = getOption("mc.cores", no_cores))
+    }
+    
+    na.list <- list()
+    na.list[[1]] <- which(matrix.1 == "9999")
+    na.list[[2]] <- which(matrix.2 == "9998")
+
+    out <- list()
+    out[["matches2"]] <- final.list
+    out[["nas"]] <- na.list
+    class(out) <- c("fastLink", "gammaKpar")
+
+    return(out)
+}
+
+## ------------------------
+## End of gamma.k.par
+## ------------------------
